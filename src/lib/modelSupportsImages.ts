f61f693c7fc3a8da48159159fadee4de8b20e567
Vitalii <0x1d46@gmail.com>
2024-09-29 14:18:36 +0200

    update_vision_models

--- src/lib/modelSupportsImages.ts
+++ src/lib/modelSupportsImages.ts
@@ -1,7 +1,8 @@
 const visionModels = [
-  "gpt-4-vision-preview",
+  "chatgpt-4o-latest",
   "gpt-4o",
   "gpt-4o-2024-05-13",
+  "gpt-4o-2024-08-06",
   "gpt-4o-mini",
   "gpt-4o-mini-2024-07-18",
 ];
